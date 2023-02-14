504c213557c31bff253129c2f36ac60e5ee98816
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-02-14 09:20:09 +0300

    Assignment Completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,48 +1,62 @@
-# your code goes here
 class Person
-    attr_reader :name, :bank_account, :happiness, :hygiene
-  
-    def initialize(name)
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+
+  def initialize(name)
       @name = name
       @bank_account = 25
       @happiness = 8
       @hygiene = 8
-    end
-  
-    def bank_account=(amount)
-      @bank_account = amount
-    end
-  
-    def happiness=(amount)
-      @happiness = [10, [0, amount].max].min
-    end
-  
-    def hygiene=(amount)
-      @hygiene = [10, [0, amount].max].min
-    end
-  
-    def happy?
-      happiness > 7
-    end
-  
-    def clean?
-      hygiene > 7
-    end
-  
-    def get_paid(amount)
-      @bank_account += amount
-      'all about the benjamins'
-    end     
+  end
 
-    def take_bath
-          self.hygiene = [self.hygiene + 4, 10].min
-          '♪ Rub-a-dub just relaxing in the tub ♫'
-    end
-      
-    def work_out
-          self.hygiene = [self.hygiene - 3, 0].max
-          self.happiness = [self.happiness + 2, 10].min
-          '♪ another one bites the dust ♫'
-    end
+  def happiness=(value)
+      @happiness = value > 10 ? 10 : value < 0 ? 0 : value
+  end
+
+  def hygiene=(value)
+      @hygiene = value > 10 ? 10 : value < 0 ? 0 : value
+  end
+
+  def happy?
+      @happiness > 7
+  end
+
+  def clean?
+      @hygiene > 7
+  end
+
+  def get_paid(salary)
+      @bank_account += salary
+      "all about the benjamins"
+  end
+
+  def take_bath
+      self.hygiene += 4
+      "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+      self.happiness += 2
+      self.hygiene -= 3
+      "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(person)
+      [self, person].each {|i| i.happiness += 3 }
+      "Hi #{person.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+      objects = [self, person]
+      if topic == "politics"
+          objects.each {|i| i.happiness -= 2 }
+          first, second = ["partisan", "lobbyist"]
+      elsif topic == "weather"
+          objects.each {|i| i.happiness += 1 }
+          first, second = ["sun", "rain"]
+      else
+          first, second = ["blah", "blah"]
+      end
+      return "blah blah #{first} blah #{second}"
+  end
 end
-  
